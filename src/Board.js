448152548af92d7d4a3f17fa7b59ab89eb603856
Snoopbobb <bobtabor@q.com>
2018-07-07 22:44:29 -0700

    finished first react app

--- src/Board.js
+++ src/Board.js
@@ -1,33 +1,71 @@
 import React, { Component } from 'react'
 import Note from './Note'
+import FaPlus from 'react-icons/lib/fa/plus'
 
 class Board extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
+            notes: []
+        }
+        this.add = this.add.bind(this)
+        this.eachNote = this.eachNote.bind(this)
+        this.update = this.update.bind(this)
+        this.remove = this.remove.bind(this)
+        this.nextId = this.nextId.bind(this)
+    }
+
+    componentWillMount() {
+        var self = this
+        if (this.props.count) {
+            fetch(`https://baconipsum.com/api/?type=all-meat&sentences=${this.props.count}`)
+                .then(response => response.json())
+                .then(json => json[0]
+                    .split('. ')
+                    .forEach(sentence => self.add(sentence.substring(0, 25))))
+        }
+    }
+
+    add(text) {
+        this.setState(prevState => ({
             notes: [
+                ...prevState.notes,
                 {
-                    id: 33,
-                    note: 'Call Shannon Sharpe'
-                },
-                {
-                    id: 34,
-                    note: 'Email John Elway'
-                },
-                {
-                    id: 35,
-                    note: 'order pizza'
+                    id: this.nextId(),
+                    note: text
                 }
             ]
-        }
-        this.eachNote = this.eachNote.bind(this)
+        }))
+    }
+
+    nextId() {
+        this.uniqueId = this.uniqueId || 0
+        return this.uniqueId++
+    }
+
+    update(newText, i) {
+        console.log('updating item at index ', i, newText)
+        this.setState(prevState => ({
+            notes: prevState.notes.map(
+                note => (note.id !== i) ? note : { ...note, note: newText }
+            )
+        }))
+    }
+
+    remove(id) {
+        console.log('removing item at ', id)
+        this.setState(prevState => ({
+            notes: prevState.notes.filter(note => note.id !== id)
+        }))
     }
 
     eachNote(note, i) {
         return (
-            <Note key={i}
-                index={i}>
+            <Note key={note.id}
+                index={note.id}
+                onChange={this.update}
+                onRemove={this.remove}>
                 {note.note}
             </Note>
         )
@@ -37,6 +75,10 @@ class Board extends Component {
         return (
             <div className="board">
                 {this.state.notes.map(this.eachNote)}
+                <button onClick={this.add.bind(null, "New Note")}
+                    id="add">
+                    <FaPlus />
+                </button>
             </div>
         )
     }
--- src/Note.js
+++ src/Note.js
@@ -15,6 +15,34 @@ class Note extends Component {
         this.save = this.save.bind(this)
         this.renderForm = this.renderForm.bind(this)
         this.renderDisplay = this.renderDisplay.bind(this)
+        this.randomBetween = this.randomBetween.bind(this)
+    }
+
+    componentWillMount() {
+        this.style = {
+            right: this.randomBetween(0, window.innerWidth - 150, 'px'),
+            top: this.randomBetween(0, window.innerWidth - 150, 'px'),
+            transform: `rotate(${this.randomBetween(-25, 25, 'deg')})`
+        }
+    }
+
+    randomBetween(x, y, s) {
+        return x + Math.ceil(Math.random() * (y - x)) + s
+    }
+
+    componentDidUpdate() {
+        var textArea
+        if (this.state.editing) {
+            textArea = this._newText
+            textArea.focus()
+            textArea.select()
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return (
+            this.props.children !== nextProps.children || this.state !== nextState
+        )
     }
 
     edit() {
@@ -24,19 +52,24 @@ class Note extends Component {
     }
 
     remove() {
-        alert('removing note')
+        this.props.onRemove(this.props.index)
     }
 
-    save() {
-        alert(this._newText.value)
+    save(e) {
+        e.preventDefault()
+        this.props.onChange(this._newText.value, this.props.index)
+        this.setState({
+            editing: false
+        })
     }
 
     renderForm() {
         return (
-            <div className="note">
-                <form>
-                    <textarea ref={input => this._newText = input} />
-                    <button onClick={this.save}><FaFloppyO /></button>
+            <div className="note" style={this.style}>
+                <form onSubmit={this.save}>
+                    <textarea ref={input => this._newText = input}
+                        defaultValue={this.props.children} />
+                    <button id="save"><FaFloppyO /></button>
                 </form>
             </div>
         )
@@ -44,7 +77,7 @@ class Note extends Component {
 
     renderDisplay() {
         return (
-            <div className="note">
+            <div className="note" style={this.style}>
                 <p>{this.props.children}</p>
                 <span>
                     <button onClick={this.edit} id="edit"><FaPencil /></button>
